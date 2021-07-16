a44ec5bfdacfce48374ad002bd69d0cc59fd5950
cbuiles <cgomezb@unal.edu.co>
2021-07-15 19:10:52 -0500

    Solucion del reto 2 encontrar la palabra mas larga

--- src/coderbyte/LongestWord.java
+++ src/coderbyte/LongestWord.java
@@ -1,21 +1,28 @@
 package coderbyte;
 
+import java.util.regex.*;
+
 public class LongestWord {
 
-	public static String LongestWord(String sen) {
-		
+	public static String LongestWord(String sen) {	
+		String longestWord = "";		
+		System.out.println(longestWord.length());
+		String reg = "(?i)[a-z]+$";
+		Pattern pat = Pattern.compile(reg);			
 		String[] arrayWords = sen.split(" ");
 		for (String string : arrayWords) {
-			System.out.println(string);
-			
+			Matcher mat = pat.matcher(string);
+			if(mat.matches() && longestWord.length()<string.length()) {
+				longestWord = string;
+			}
 		}
-		return sen;				
+		return longestWord;				
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(LongestWord("HOla mundo"));
+		System.out.println(LongestWord("i love dogs"));
 		
 	}
 
