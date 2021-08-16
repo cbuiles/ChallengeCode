c787663795ee47dd21cb5ca7e4bcbfe404f376ec
cbuiles <cgomezb@unal.edu.co>
2021-08-16 11:30:13 -0500

    Reto Java Generics HackerRank

---
+++ src/HackerRank/JavaGenerics.java
@@ -0,0 +1,38 @@
+package HackerRank;
+
+import java.lang.reflect.Method;
+
+class Printer{
+	
+	static <T> void printArray (T[] array) {
+		
+		for (T element : array) {
+			System.out.println(element);
+		}
+	}
+}
+
+public class JavaGenerics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+
+	}
+
+}
