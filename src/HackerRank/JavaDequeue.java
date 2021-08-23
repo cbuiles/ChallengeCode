915b6419d18c1c926b7c4aff378d92540e8c2c09
cbuiles <cgomezb@unal.edu.co>
2021-08-22 23:33:18 -0500

    Java Deque HackerRank

---
+++ src/HackerRank/JavaDequeue.java
@@ -0,0 +1,41 @@
+package HackerRank;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.Scanner;
+import java.util.Set;
+
+public class JavaDequeue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+        Deque<Integer> deque = new ArrayDeque<>();
+        HashSet<Integer> set = new HashSet<>();
+        
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int max =0;
+
+        for (int i = 0; i < n; i++) {
+            int input = in.nextInt();
+            
+            deque.add(input);
+            set.add(input);
+            
+            if (deque.size() == m) {
+                if (set.size() > max) max = set.size();
+                int first = deque.remove();
+                if (!deque.contains(first)) set.remove(first);
+            }
+        }
+        
+        System.out.println(max);
+		
+	}
+
+}
